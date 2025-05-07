190c74c0b2d950961fc05257121ee70c493c4a26
Ganesh M <ganeshmeti2004@gmail.com>
2025-05-07 11:35:07 +0530

    CORS configured

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,11 @@ import sequenceRouter from "./routes/sequence.route.js";
 const app = express();
 
 app.use(cors({
-  origin:process.env.ORIGIN,
+  origin: [
+    'https://automatic-email-scheduler.vercel.app',
+    'https://automatic-email-scheduler-rh87.vercel.app',
+    'http://localhost:5173'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
@@ -16,7 +20,7 @@ app.set("trust proxy", 1);
 app.use(express.static("public"));
 app.use(express.json({ limit: "16kb" }));
 
-app.use("api/sequence", sequenceRouter);
+app.use("/api/sequence", sequenceRouter);
 
 const __dirname = path.resolve();
 

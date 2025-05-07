efd56aa127bfff90df733ab3b2fca0953db7c499
Ganesh M <ganeshmeti2004@gmail.com>
2025-05-07 10:19:35 +0530

    CORS updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ import sequenceRouter from "./routes/sequence.route.js";
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin:process.env.ORIGIN,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
